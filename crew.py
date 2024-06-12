804ef4825fa4b4868495f0709df697841a02dab8
Harisudhan5 <speaktoharisudhan@gmail.com>
2024-06-12 19:37:48 +0530

    created agent 1

---
+++ crew.py
@@ -0,0 +1,37 @@
+from crewai import Agent
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model = '', verbose = True, temperature = 0.5, google_api_key = os.getenv('GOOGLE_API_KEY'))
+
+
+# Agent for discovering latest news about the given topic
+
+researcher = Agent(
+    role = "Senior Researcher",
+    goal = "Uncover Latest news about {topic}",
+    verbose = True,
+    memory = True,
+    backstory = (
+        "Driven by curiosity, you're at the forefront of"
+"innovation, eager to explore and share knowledge that could change"
+"the world."
+    ),
+    tools = [],
+    llm = llm,
+    allow_delegation = True
+)
+
+
+# writing agent for detailed report about the contents from researcher
+
+news_writer = Agent(
+    role = 'Writer',
+    goal = "Narrate details to know more about {topic}",
+    verbose = True,
+    memory = True
+)
